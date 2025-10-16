aa76967bc678ed227144b677c3f5c5d9e0568288
Vikorikus <Vikorikus@gmail.com>
2025-10-16 20:21:17 +0300

    first commit react kata

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  plugins: ["react", "@typescript-eslint"],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
+};
